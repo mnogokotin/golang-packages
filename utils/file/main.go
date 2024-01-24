2e5ef2132c2d9a65e389b894cb6fc396e50db8b9
mnogokotin <mnogokotin@gmail.com>
2024-01-24 17:28:09 +0400

    utils file getLastLine

---
+++ utils/file/main.go
@@ -0,0 +1,33 @@
+package file
+
+import (
+	"fmt"
+	"io"
+	"os"
+)
+
+func getLastLine(f *os.File) string {
+	line := ""
+	var cursor int64 = 0
+	stat, _ := f.Stat()
+	filesize := stat.Size()
+	for {
+		cursor -= 1
+		f.Seek(cursor, io.SeekEnd)
+
+		char := make([]byte, 1)
+		f.Read(char)
+
+		if cursor != -1 && (char[0] == 10 || char[0] == 13) { // stop if we find a line
+			break
+		}
+
+		line = fmt.Sprintf("%s%s", string(char), line) // there is more efficient way
+
+		if cursor == -filesize { // stop if we are at the begining
+			break
+		}
+	}
+
+	return line
+}
