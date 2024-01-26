9f0397de2f5d2e8e7259ec631d562a7ce273fe50
mnogokotin <mnogokotin@gmail.com>
2024-01-26 20:00:33 +0400

    utils file enchancement

--- utils/file/main.go
+++ utils/file/main.go
@@ -1,12 +1,13 @@
 package file
 
 import (
+	"errors"
 	"fmt"
 	"io"
 	"os"
 )
 
-func GetLastLine(f *os.File) string {
+func GetLastLine(f *os.File) (string, error) {
 	line := ""
 	var cursor int64 = 0
 	stat, _ := f.Stat()
@@ -18,16 +19,20 @@ func GetLastLine(f *os.File) string {
 		char := make([]byte, 1)
 		f.Read(char)
 
+		if char[0] == 0 { // stop if file is empty
+			return "", errors.New("file is empty")
+		}
+
 		if cursor != -1 && (char[0] == 10 || char[0] == 13) { // stop if we find a line
 			break
 		}
 
 		line = fmt.Sprintf("%s%s", string(char), line) // there is more efficient way
 
-		if cursor == -filesize { // stop if we are at the begining
+		if cursor == -filesize { // stop if we are at the beginning
 			break
 		}
 	}
 
-	return line
+	return line, nil
 }
